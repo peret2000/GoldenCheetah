d620611f634d12a225242dbf9745d817c514e225
Peret <19463568+peret2000@users.noreply.github.com>
2023-05-14 10:45:00 +0200

    Añadido script compile_and_continue.sh
    Reproduce la parte final del script general de compilación: sólo compila y crea el AppImage

---
+++ scripts/compile_and_continue.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Script para ganerar AppImage y extraer su contenido, cuando sólo se necesita compilar cambios
+# Compila los cambios y genera AppImage
+
+# Check whether .bashrc has been loaded (for example, cron does not load it)
+if [[ -z "${ENV_LOADED}" ]]; then
+        source $HOME/.profile
+fi
+
+
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+export LOGFILE=$SCRIPT_DIR/log.txt
+
+cd $SCRIPT_DIR
+
+echo ------------------------- >> $LOGFILE
+echo Compilación sólo!!!!! >> $LOGFILE
+echo Comienzo: `date` >> $LOGFILE
+
+cd GoldenCheetah_Debug
+
+make -j1                          && echo "Compile OK" >> $LOGFILE || echo "Compile FAILED" >> $LOGFILE
+
+# Generate the AppImage
+
+echo after_success.sh: `date` >> $LOGFILE
+
+sed -i '/free.keep.sh/s/^/echo Commented out:/' travis/linux/after_success.sh 
+
+[[ -d src/appdir ]] && rm -rf src/appdir
+[[ -d squashfs-root ]] && rm -rf squashfs-root
+
+[[ -f src/GoldenCheetah_v3.6-DEV_x64.AppImage ]] && rm src/GoldenCheetah_v3.6-DEV_x64.AppImage
+travis/linux/after_success.sh                           && echo "deploy OK" >> $LOGFILE || echo "deploy FAILED" >> $LOGFILE
+
+src/GoldenCheetah_v3.6-DEV_x64.AppImage --appimage-extract
+
