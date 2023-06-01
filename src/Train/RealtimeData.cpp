765d095512f91b8c4527d450a7aa4a6e48e773f2
Peret <19463568+peret2000@users.noreply.github.com>
2023-06-01 19:48:18 +0200

    Añadido RealTime data: Gear
    Añadido sin considerar cambios de pendiente (inercia y energía potencial). Se deja pendiente descontar ese efecto
    
    Se admite el widget de vídeo 'Gear', cuando su campo 'name' es 'GearPlato44', calcula el piñón que le corresponde

--- src/Train/RealtimeData.cpp
+++ src/Train/RealtimeData.cpp
@@ -25,7 +25,7 @@
 RealtimeData::RealtimeData()
 {
     name[0] = '\0';
-    hr= watts= avgWatts = altWatts= speed= wheelRpm= load= slope= torque= 0.0;
+    hr= watts= avgWatts = altWatts= speed= wheelRpm= load= slope= torque= gear= 0.0;
     cadence = distance = altDistance = virtualSpeed = avgSpeed = wbal = 0.0;
     lap = msecs = lapMsecs = lapMsecsRemaining = ergMsecsRemaining = 0;
     thb = smo2 = o2hb = hhb = 0.0;
@@ -64,6 +64,10 @@ void RealtimeData::setAltWatts(double watts)
 void RealtimeData::setAvgWatts(double avgWatts)
 {
     this->avgWatts= avgWatts;
+}
+void RealtimeData::setGear(double gear)
+{
+    this->gear= gear;
 }
 void RealtimeData::setJoules(long joules)
 {
@@ -233,6 +237,10 @@ double RealtimeData::getWatts() const
 double RealtimeData::getAvgWatts() const
 {
     return avgWatts;
+}
+double RealtimeData::getGear() const
+{
+    return gear;
 }
 double RealtimeData::getHr() const
 {
@@ -502,6 +510,9 @@ double RealtimeData::value(DataSeries series) const
     case AvgWatts: return avgWatts;
         break;
 
+    case Gear: return gear;
+        break;
+
     case Watts: return watts;
         break;
 
@@ -693,6 +704,7 @@ const QList<RealtimeData::DataSeries> &RealtimeData::listDataSeries()
         seriesList << Bearing;
         seriesList << DeltaSlope;
         seriesList << ElevationGain;
+        seriesList << Gear;
     }
     return seriesList;
 }
@@ -787,6 +799,9 @@ QString RealtimeData::seriesName(DataSeries series)
         break;
 
     case AvgWatts: return tr("Average Power");
+        break;
+
+    case Gear: return tr("Gear");
         break;
 
     case AvgSpeed: return tr("Average Speed");
