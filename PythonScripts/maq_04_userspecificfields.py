a9dff20e60139662b33f6eabb6a9c1bf7140c9d8
MAQ_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2023-06-27 17:24:49 +0200

    Campos específicos de usuario: pool length y weight
    Si es natación en piscina, se pone una longitud por defecto de 10m
    
    Se pone el peso a 0, para que tome el de GoldenCheetah. Garmin pone el tag peso a lo que tiene configurado

--- PythonScripts/maq_04_userspecificfields.py
+++ PythonScripts/maq_04_userspecificfields.py
@@ -9,10 +9,15 @@ def main():
 	device = GC.getTag('Device')
 
 	if GC.athlete()['name'] == 'MiguelAngel':
+		# Si viene de Garmin, pone el peso que tiene configurado
+		GC.setTag('Weight', '0')
 		# Si SubSport='VirtualRun' -> InclCinta = 2
 		if subsport == 'VirtualRun' and not (device=='Kinomap' or device=='Kinomap Run'):
 			GC.setTag('InclCinta', '2')
 			print(GC.getTag("InclCinta"))
+		# Si SubSport='lap swimming' -> Longitud de piscina = 10 (piscina de la comunidad)
+		if subsport == 'lap swimming':
+			GC.setTag('Pool Length', '10')
 	elif GC.athlete()['name'] == 'Julio':
 		# Si SubSport='VirtualRun' -> InclCinta = 1
 		if subsport == 'VirtualRun' and not (device=='Kinomap' or device=='Kinomap Run'):
