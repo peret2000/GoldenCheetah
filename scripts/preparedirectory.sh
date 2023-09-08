0f98b049300116313795fa9b4eefe986a50627c0
MAQ_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2023-09-08 14:34:41 +0200

    preparedirectory.sh carga .profile y modifica script.sh
    También se modifica compile_and_continue.sh para compilar con el número de procesadores físicos

--- scripts/preparedirectory.sh
+++ scripts/preparedirectory.sh
@@ -5,9 +5,17 @@
 
 
 # Prepara el directorio pasado como argumento para compilar por primera vez
+
+# Si se ejecuta el script diario (daily_script.sh), ya prepara el directorio, para compilación release.
+# Si no, es conveniente ejecutar este script, para preparar una compilación, tanto de QtCreator como manual
+
 # Ejecuta travis/linux/before_script.sh (debe estar la variable de entorno $GC_STRAVA_CLIENT_SECRET)
 # modifica gcconfig.pri
 
+# Check whether .bashrc has been loaded (for example, cron does not load it)
+if [[ -z "${ENV_LOADED}" ]]; then
+        source $HOME/.profile
+fi
 
 # Aquí se debe poner la variables de entorno $GC_STRAVA_CLIENT_SECRET (o existir ya) si se quiere compilar con ella
 
@@ -22,4 +30,5 @@ sed -i '/-O3/ d' src/gcconfig.pri
 
 echo CONFIG += debug static >> src/gcconfig.pri
 
-
+# El make usa tantos procesos como procesadores físicos
+sed -i "s/-j4/-j$(lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l)/" travis/linux/script.sh
