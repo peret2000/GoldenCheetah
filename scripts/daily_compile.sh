a441e2444ff51820f0f2c92fdcef6c88e1a212ba
MA_Oracle <19463568+peret2000@users.noreply.github.com>
2024-04-15 13:51:30 +0200

    Scripts corregidos para volcar resultado compilación en fichero log

--- scripts/daily_compile.sh
+++ scripts/daily_compile.sh
@@ -85,7 +85,8 @@ echo script.sh: `date` >> $LOGFILE
 
 # Ésta es una forma 'compleja' de ejecutar un comando, que muestre la salida por pantalla, además de escribir en un fichero, y utilizar
 # el código de error de la salida (process substitution)
-./travis/linux/script.sh > >(tee -a $BUILDLOG) 2> >(tee -a $BUILDLOG >&2) && { echo "Compile OK"; } || { ERR=$?; echo "Compile FAILED"; salida $ERR;}
+./travis/linux/script.sh > >(tee -a $BUILDLOG) 2> >(tee -a $BUILDLOG >&2) \
+	&& { echo "Compile OK" >> $LOGFILE;} || { ERR=$?; echo "Compile FAILED" >> $LOGFILE; salida $ERR;}
 echo ------------------------- >> $BUILDLOG
 echo Finalización: `date` >> $BUILDLOG
 
