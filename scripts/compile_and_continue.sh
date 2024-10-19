ae31af0d2eed220ca1656544804aeea2024f80e3
MA_Oracle <19463568+peret2000@users.noreply.github.com>
2024-10-19 17:27:52 +0200

    Mejora salida logs de compile_and_continue.sh

--- scripts/compile_and_continue.sh
+++ scripts/compile_and_continue.sh
@@ -9,7 +9,9 @@ if [[ -z "${ENV_LOADED}" ]]; then
 fi
 
 salida() {
-        scripts/pushover_end_compile.sh "Compile and Continue" $LOGFILE
+	[[ -n "$1" && "$1" != "0" ]] && echo ">>>EJECUCIÓN FALLIDA: $1" | tee -a $LOGFILE
+        scripts/pushover_end_compile.sh "Compile and Continue $HOSTNAME" $LOGFILE > /dev/null 2>&1
+	echo Termina: `date` | tee -a $LOGFILE
         cat $LOGFILE >> $CUMLOGFILE
         rm $LOGFILE
         exit $1
@@ -24,22 +26,20 @@ export BUILDLOG=$SCRIPT_DIR/buildlog.txt
 echo Comienzo: `date` > $BUILDLOG
 echo ------------------------- >> $BUILDLOG
 
-echo ------------------------- > $LOGFILE
-echo Compilación sólo!!!!! >> $LOGFILE
-echo Comienzo: `date` >> $LOGFILE
+echo ------------------------- | tee $LOGFILE
+echo Compilación sólo!!!!! | tee -a $LOGFILE
+echo Comienzo: `date` | tee -a $LOGFILE
 
 cd $SCRIPT_DIR/..
 
-# Ésta es una forma 'compleja' de ejecutar un comando, que muestre la salida por pantalla, además de escribir en un fichero, y utilizar
-# el código de error de la salida (process substitution)
-./travis/linux/script.sh > >(tee -a $BUILDLOG) 2> >(tee -a $BUILDLOG >&2) \
-		&& { echo "Compile OK"; >> $LOGFILE;} || { ERR=$?; echo "Compile FAILED" >> $LOGFILE; salida $ERR;}
+./travis/linux/script.sh >> $BUILDLOG 2>&1 && { echo "Compile OK" | tee -a $LOGFILE; } || { ERR=$?; echo "ERROR: Compile FAILED" | tee -a $LOGFILE; salida $ERR; }
+
 echo ------------------------- >> $BUILDLOG
 echo Finalización: `date` >> $BUILDLOG
 
 # Generate the AppImage
 
-echo after_success.sh: `date` >> $LOGFILE
+echo after_success.sh: `date` | tee -a $LOGFILE
 
 sed -i '/free.keep.sh/s/^/echo Commented out:/' travis/linux/after_success.sh 
 sed -i 's/sudo //' travis/linux/after_success.sh
@@ -49,8 +49,8 @@ sed -i 's/git log -1 >> GCversionLinux.txt/git merge-base HEAD  goldencheetah\/m
 [[ -d squashfs-root ]] && rm -rf squashfs-root
 
 [[ -f src/GoldenCheetah_v3.7-DEV_x64.AppImage ]] && rm src/GoldenCheetah_v3.7-DEV_x64.AppImage
-travis/linux/after_success.sh && { echo "deploy OK" >> $LOGFILE; } || { ERR=$?; echo "deploy FAILED" >> $LOGFILE; salida $ERR;}
+travis/linux/after_success.sh > /dev/null 2>&1 && { echo "deploy OK" | tee -a $LOGFILE; } || { ERR=$?; echo "ERROR: deploy FAILED" | tee -a $LOGFILE; salida $ERR; }
 
-src/GoldenCheetah_v3.7-DEV_x64.AppImage --appimage-extract
+src/GoldenCheetah_v3.7-DEV_x64.AppImage --appimage-extract > /dev/null 2>&1
 
 salida 0
