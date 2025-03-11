5212e1fdbc2234e249ae574ca8a2ead49376fa5d
MA_Docker <19463568+peret2000@users.noreply.github.com>
2025-03-11 16:56:11 +0100

    Corrige 0522b6c97. Error de sintaxis

--- src/Train/WebPageWindow.cpp
+++ src/Train/WebPageWindow.cpp
@@ -203,7 +203,7 @@ WebPageWindow::userUrl()
 {
     // add http:// if scheme is missing
     QRegExp hasscheme("^[^:]*://.*");
-    QString url = CustomUrl->text();
+    QString url = customUrl->text();
     if (!hasscheme.exactMatch(url)) url = "http://" + url;
     view->setZoomFactor(dpiXFactor);
     view->setUrl(QUrl(url));
