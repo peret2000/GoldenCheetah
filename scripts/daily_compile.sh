b354ddedfbf242450b84b93fbe639b25fb8d4164
MA_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2025-04-01 13:59:10 +0200

    Continuación de 72293c033
    Continúa cambios de nombre de appimage en otros lugares

--- scripts/daily_compile.sh
+++ scripts/daily_compile.sh
@@ -125,7 +125,7 @@ sed -i 's/git log -1 >> GCversionLinux.txt/git merge-base HEAD  goldencheetah\/m
 [[ -d src/appdir ]] && rm -rf src/appdir
 [[ -d squashfs-root ]] && rm -rf squashfs-root
 
-[[ -f src/GoldenCheetah_v3.7-DEV_x64.AppImage ]] && rm src/GoldenCheetah_v3.7-DEV_x64.AppImage
+ls src/GoldenCheetah*.AppImage >/dev/null 2>&1 && rm src/GoldenCheetah*.AppImage
 travis/linux/after_success.sh > /dev/null 2>&1 && { echo "deploy OK" | tee -a $LOGFILE; } || { ERR=$?; echo "ERROR: deploy FAILED" | tee -a $LOGFILE; salida $ERR; }
 
 src/GoldenCheetah_v3.7_x64.AppImage --appimage-extract > /dev/null 2>&1
