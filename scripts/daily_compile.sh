e0d99b706c8afadb700bc17ac2b3f53567dff4ba
MA_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2025-05-29 20:34:41 +0200

    Renombrado de fichero appimage
    Se adapta el script de compilación diaria al cambio de nombre del fichero appimage, que se introdujo para ir avanzando con otras formas de generar compilaciones del proyecto

--- scripts/daily_compile.sh
+++ scripts/daily_compile.sh
@@ -128,6 +128,6 @@ sed -i 's/git log -1 >> GCversionLinux.txt/git merge-base HEAD  goldencheetah\/m
 ls src/GoldenCheetah*.AppImage >/dev/null 2>&1 && rm src/GoldenCheetah*.AppImage
 travis/linux/after_success.sh > /dev/null 2>&1 && { echo "deploy OK" | tee -a $LOGFILE; } || { ERR=$?; echo "ERROR: deploy FAILED" | tee -a $LOGFILE; salida $ERR; }
 
-src/GoldenCheetah_v3.7_x64.AppImage --appimage-extract > /dev/null 2>&1
+src/GoldenCheetah_v3.7_x64Old.AppImage --appimage-extract > /dev/null 2>&1
 
 salida 0
