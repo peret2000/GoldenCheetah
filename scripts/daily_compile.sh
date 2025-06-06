c44e8f9a6579534504bca03fac94033dbf33215f
MA_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2025-06-06 18:58:21 +0200

    Script diario unificado con compile_and_continue
    También se modifica .gitignore para que no monitorice src/GCversionLinuxQt6.txt

--- scripts/daily_compile.sh
+++ scripts/daily_compile.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Argument (optional): branch to merge with
-
+# Usage: ./daily_compile.sh [--appimage] [--justcompile] [--help|-h]
 
 # Check whether .bashrc has been loaded (for example, cron does not load it)
 if [[ -z "${ENV_LOADED}" ]]; then
@@ -10,7 +9,7 @@ fi
 
 salida() {
 	[[ -n "$1" && "$1" != "0" ]] && echo ">>>EJECUCIÓN FALLIDA: $1" | tee -a $LOGFILE
-	scripts/pushover_end_compile.sh "Nightly Build $HOSTNAME" $LOGFILE > /dev/null 2>&1
+	scripts/pushover_end_compile.sh "$TEXT $HOSTNAME" $LOGFILE > /dev/null 2>&1
 	echo Termina: `date` | tee -a $LOGFILE
 	cat $LOGFILE >> $CUMLOGFILE
 	rm $LOGFILE
@@ -24,6 +23,46 @@ export LOGFILE=$SCRIPT_DIR/logtmp.txt
 export CUMLOGFILE=$SCRIPT_DIR/log.txt
 export BUILDLOG=$SCRIPT_DIR/buildlog.txt
 
+APPIMAGE=false
+JUSTCOMPILE=false
+
+# Script command line help
+mostrar_ayuda() {
+    echo "Usage: $0 [options]"
+    echo ""
+    echo "Options:"
+    echo "  --appimage      Creates the appimage file"
+    echo "  --justcompile   Does not build from scratch, just compiles the code"
+	echo "  --help, -h     	Shows this help message"
+    echo ""
+    exit 1
+}
+
+TEXT="Compilación diaria de GoldenCheetah"
+
+# Procesamos los argumentos
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --appimage)
+            APPIMAGE=true
+            shift
+            ;;
+        --justcompile)
+            JUSTCOMPILE=true
+			TEXT="Compilación sólo!!!!!"
+            shift
+            ;;
+        --help|-h)
+            mostrar_ayuda
+            ;;
+        *)
+            echo "Opción desconocida: $1"
+            mostrar_ayuda
+            ;;
+    esac
+done
+
+
 # $2 es opcional. Para hacer merge en una rama que no es la actual
 merge() {
 git merge --no-edit $1 $2 > /dev/null 2>&1 || {
@@ -31,79 +70,79 @@ git merge --no-edit $1 $2 > /dev/null 2>&1 || {
         } && echo "merge $1 OK" | tee -a $LOGFILE
 }
 
-echo ------------------------- | tee $LOGFILE
-echo Comienzo: `date` | tee -a $LOGFILE
-echo git fetch, merge, etc | tee -a $LOGFILE
+echo ------------------------- | tee $LOGFILE $BUILDLOG
+echo Comienzo: `date` | tee -a $LOGFILE $BUILDLOG
+echo $TEXT | tee -a $LOGFILE
 
 ###cd $SCRIPT_DIR/../.. && git clone git@github.com:peret2000/GoldenCheetah.git GoldenCheetah
 
 cd $SCRIPT_DIR/..
 
-###git remote add goldencheetah https://github.com/GoldenCheetah/GoldenCheetah.git
+if ! $JUSTCOMPILE; then
 
-# Esto no debería ser necesario si se hace un git clone, partiendo de cero
-# Es por si el repositorio se quedó con un merge a medias, por ejemplo, por un conflicto
-# Si no había conflicto, dará un error que se puede ignorar
-git merge --abort > /dev/null 2>&1
+	echo git fetch, merge, etc | tee -a $LOGFILE
 
-git fetch --all
+	###git remote add goldencheetah https://github.com/GoldenCheetah/GoldenCheetah.git
 
-# Estos ficheros se modifican en la compilación y pueden dar problemas al hacer merge
-git checkout -- src/Resources/translations/
-git checkout -- src/Core/Secrets.h
-git checkout -- travis/linux/script.sh
-git checkout -- travis/linux/after_success.sh
+	# Esto no debería ser necesario si se hace un git clone, partiendo de cero
+	# Es por si el repositorio se quedó con un merge a medias, por ejemplo, por un conflicto
+	# Si no había conflicto, dará un error que se puede ignorar
+	git merge --abort > /dev/null 2>&1
 
-git checkout MyBuildAdapt
-# Chequea que esté en la última versión
-COMMIT_BEFORE=$(git rev-parse HEAD)
-git merge || { ERR=$?; echo "Unable to merge MyBuildAdapt, Maybe branch has diverged. Process FAILED." | tee -a $LOGFILE; salida $ERR; }
-COMMIT_AFTER=$(git rev-parse HEAD)
-if [ "$COMMIT_BEFORE" != "$COMMIT_AFTER" ]; then
-	echo "FAILED. MyBuildAdapt NOT in last version." | tee -a $LOGFILE
-	salida $ERR
-fi
+	git fetch --all
 
-# Por si existe ya la rama, primero se elimina
-git branch -D NightlyBuild
-git checkout -b NightlyBuild
+	# Estos ficheros se modifican en la compilación y pueden dar problemas al hacer merge
+	git checkout -- src/Resources/translations/
+	git checkout -- src/Core/Secrets.h
+	git checkout -- travis/linux/script.sh
+	git checkout -- travis/linux/after_success.sh
+
+	git checkout MyBuildAdapt
+	# Chequea que esté en la última versión
+	COMMIT_BEFORE=$(git rev-parse HEAD)
+	git merge || { ERR=$?; echo "Unable to merge MyBuildAdapt, Maybe branch has diverged. Process FAILED." | tee -a $LOGFILE; salida $ERR; }
+	COMMIT_AFTER=$(git rev-parse HEAD)
+	if [ "$COMMIT_BEFORE" != "$COMMIT_AFTER" ]; then
+		echo "FAILED. MyBuildAdapt NOT in last version." | tee -a $LOGFILE
+		salida $ERR
+	fi
+
+	# Por si existe ya la rama, primero se elimina
+	git branch -D NightlyBuild
+	git checkout -b NightlyBuild
 
 
-merge origin/TrainButtons
-merge origin/MyZEW
-merge origin/VideoWidgets
-merge origin/SmoothPowerEstim
-merge origin/Strava
-merge origin/PyAutomatedProcessors
-merge origin/treadmill_qdomyos
-merge origin/utils
-merge origin/train_view_improvements
-merge origin/activities_view_improvements
-merge origin/train_geolocation_widget
+	merge origin/TrainButtons
+	merge origin/MyZEW
+	merge origin/VideoWidgets
+	merge origin/SmoothPowerEstim
+	merge origin/Strava
+	merge origin/PyAutomatedProcessors
+	merge origin/treadmill_qdomyos
+	merge origin/utils
+	merge origin/train_view_improvements
+	merge origin/activities_view_improvements
+	merge origin/train_geolocation_widget
 
-merge goldencheetah/master
+	merge goldencheetah/master
 
-#### Merge temporal del PR4533: Equipment management feature tiled
-# La ramo paulj49457/equipment_feature_tiled ya no existe, por lo que el merge para actualizar esa rama
-# ya no se hace. La rama origin/tmp_equipment_feature_tiled contiene esa rama desaparecida
-#git remote add paulj49457 https://github.com/paulj49457/GoldenCheetah.git > /dev/null 2>&1
-#git fetch paulj49457
-#merge paulj49457/equipment_feature_tiled origin/tmp_equipment_feature_tiled
-merge origin/tmp_equipment_feature_tiled
-##############################
+	#### Merge temporal del PR4533: Equipment management feature tiled
+	# La ramo paulj49457/equipment_feature_tiled ya no existe, por lo que el merge para actualizar esa rama
+	# ya no se hace. La rama origin/tmp_equipment_feature_tiled contiene esa rama desaparecida
+	#git remote add paulj49457 https://github.com/paulj49457/GoldenCheetah.git > /dev/null 2>&1
+	#git fetch paulj49457
+	#merge paulj49457/equipment_feature_tiled origin/tmp_equipment_feature_tiled
+	merge origin/tmp_equipment_feature_tiled
+	##############################
 
-if [ "$1" ]; then
-  merge origin/$1
-fi
+	echo preparedirectory.sh: `date` | tee -a $LOGFILE
 
-echo preparedirectory.sh: `date` | tee -a $LOGFILE
+	./scripts/preparedirectory.sh > /dev/null 2>&1 && { echo "preparedirectory OK" | tee -a $LOGFILE; } || { ERR=$?; echo "preparedirectory FAILED" | tee -a $LOGFILE; salida $ERR; }
 
-./scripts/preparedirectory.sh > /dev/null 2>&1 && { echo "preparedirectory OK" | tee -a $LOGFILE; } || { ERR=$?; echo "preparedirectory FAILED" | tee -a $LOGFILE; salida $ERR; }
+fi	# if ! $JUSTCOMPILE; then
 
 echo script.sh: `date` | tee -a $LOGFILE
 
-echo Comienzo: `date` > $BUILDLOG
-echo ------------------------- >> $BUILDLOG
 ### Ésta es una forma 'compleja' de ejecutar un comando, que muestre la salida por pantalla, además de escribir en un fichero, y utilizar
 ### el código de error de la salida (process substitution)
 ##./travis/linux/script.sh > >(tee -a $BUILDLOG) 2> >(tee -a $BUILDLOG >&2) \
@@ -114,17 +153,40 @@ echo ------------------------- >> $BUILDLOG
 echo ------------------------- >> $BUILDLOG
 echo Finalización: `date` >> $BUILDLOG
 
-# Generate the AppImage
-
-echo MakeAppImageQt6.sh: `date` | tee -a $LOGFILE
-
 [[ -d src/appdir ]] && rm -rf src/appdir
-[[ -d squashfs-root ]] && rm -rf squashfs-root
 
-ls src/GoldenCheetah*.AppImage >/dev/null 2>&1 && rm src/GoldenCheetah*.AppImage
-cd src
-./Resources/linux/MakeAppImageQt6.sh > /dev/null 2>&1 && { echo "deploy OK" | tee -a $LOGFILE; } || { ERR=$?; echo "ERROR: deploy FAILED" | tee -a $LOGFILE; salida $ERR; }
-cd ..
-src/GoldenCheetah_v3.7_x64Qt6.AppImage --appimage-extract > /dev/null 2>&1
+if ! $APPIMAGE; then
+
+	echo genera binario con linuxdeployqt: `date` | tee -a $LOGFILE
+	# Download current version of linuxdeployqt
+	cd src
+	wget --no-verbose -c https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+	chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+	# Deploy to appdir
+	mkdir -p appdir
+	cp -p GoldenCheetah appdir/
+	# Lightweight deploy
+	./linuxdeployqt-continuous-x86_64.AppImage appdir/GoldenCheetah -verbose=2 -exclude-libs=libqsqlmysql,libqsqlpsql,libqsqlmimer,libqsqlodbc,libnss3,libnssutil3,libxcb-dri3.so.0 \
+			-unsupported-allow-new-glibc -no-translations -no-plugins -no-copy-copyright-files -no-strip -qmake=/usr/bin/qmake6
+	mkdir -p ../squashfs-root && mv appdir/GoldenCheetah ../squashfs-root/
+	# Cleanup
+	rm linuxdeployqt-continuous-x86_64.AppImage
+	rm -rf ./appdir
+
+else
+
+	# Generate the AppImage
+
+	echo MakeAppImageQt6.sh: `date` | tee -a $LOGFILE
+
+	[[ -d squashfs-root ]] && rm -rf squashfs-root
+
+	ls src/GoldenCheetah*.AppImage >/dev/null 2>&1 && rm src/GoldenCheetah*.AppImage
+	cd src
+	./Resources/linux/MakeAppImageQt6.sh > /dev/null 2>&1 && { echo "deploy OK" | tee -a $LOGFILE; } || { ERR=$?; echo "ERROR: deploy FAILED" | tee -a $LOGFILE; salida $ERR; }
+	cd ..
+	src/GoldenCheetah_v3.7_x64Qt6.AppImage --appimage-extract > /dev/null 2>&1
+
+fi
 
 salida 0
