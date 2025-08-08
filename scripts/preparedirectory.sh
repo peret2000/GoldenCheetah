948b56e3abd5b36d80cdc1d0ed566ba6fc3def2c
MA_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2025-08-08 14:21:10 +0200

    Continuación de #ec6a07a94
    Añadido el mismo parámetro para el script appimagetool-x86_64.AppImage

--- scripts/preparedirectory.sh
+++ scripts/preparedirectory.sh
@@ -92,5 +92,13 @@ sed -i '
 }
 ' src/Resources/linux/MakeAppImageQt6.sh
 
+sed -i '
+/^\.\/appimagetool.*AppImage/ {
+    /--appimage-extract-and-run/ ! {
+        s/\(\.\/appimagetool[^[:space:]]*AppImage\)/\1 --appimage-extract-and-run/
+    }
+}
+' src/Resources/linux/MakeAppImageQt6.sh
+
 sed -i 's|^cp -r `qmake.*$|cp -r /usr/share/qt6/resources appdir|' src/Resources/linux/MakeAppImageQt6.sh
 sed -i 's/git log -1 >> GCversionLinuxQt6.txt/git merge-base HEAD  goldencheetah\/master |xargs git log -1>>GCversionLinuxQt6.txt/' src/Resources/linux/MakeAppImageQt6.sh
