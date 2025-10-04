b3aa624672a8ec8e5eeee500b8b549c0cb1bafc7
MA_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2025-10-04 11:12:03 +0200

    Corrije 6260d7f93

--- src/Resources/linux/MakeAppImageQt6.sh
+++ src/Resources/linux/MakeAppImageQt6.sh
@@ -59,7 +59,8 @@ PYTHON37DIR="$(dirname "$(dirname "$(command -v python3.7)")")"
 export PATH="$PYTHON37DIR/bin:$PATH"
 pip install --upgrade pip
 pip install -q -r Python/requirements.txt
-cp -rp $PYTHON37DIR appdir/opt/python3.7
+mkdir -p appdir/opt/python3.7
+cp -rp $PYTHON37DIR appdir/opt/python3.7/
 
 # Fix RPATH on QtWebEngineProcess and copy missing resources
 patchelf --set-rpath '$ORIGIN/../lib' appdir/libexec/QtWebEngineProcess
