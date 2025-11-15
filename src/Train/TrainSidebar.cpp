6aaf36c37b29f0cb0641aa9c5d5f9e4e7b22c0ef
MA_Docker <19463568+peret2000@users.noreply.github.com>
2025-11-15 17:53:53 +0100

    Corrige 73392cac6. Si no hay pendiente, no calcula delta slope
    Evita un warning continuo en el log

--- src/Train/TrainSidebar.cpp
+++ src/Train/TrainSidebar.cpp
@@ -2084,7 +2084,7 @@ void TrainSidebar::guiUpdate()           // refreshes the telemetry
 
                 }
 
-                {
+                if (ergFile->hasGradient()) {
                     // Average slope in 10 seconds (taking into account current speed)
 
                     int lap;
@@ -2101,6 +2101,8 @@ void TrainSidebar::guiUpdate()           // refreshes the telemetry
                     displayDeltaSlope = deltaSlope - rtData.getSlope();
                     rtData.setDeltaSlope(displayDeltaSlope);
                 }
+                else
+                   rtData.setDeltaSlope(0.0);
 
                     // Elevation Gain
                 {
