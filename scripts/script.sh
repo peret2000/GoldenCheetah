aa6a285d3ca3df30f18682dbc943ad77bf20ac6e
MA_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2025-12-13 21:05:28 +0100

    Corrige error en script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,8 +4,6 @@ set -ev
 # Substituye al deprecado travis/linux/script.sh (#84b26c597)
 # Replica lo que se hace en appveyor.yml
 
-qmake build.pro -r QMAKE_CXXFLAGS_WARN_ON+=\
-	"-Wno-unused-private-field -Wno-c++11-narrowing -Wno-deprecated-declarations -Wno-deprecated-register -Wno-nullability-completeness -Wno-sign-compare -Wno-inconsistent-missing-override" \
-	QMAKE_CFLAGS_WARN_ON+="-Wno-deprecated-declarations -Wno-sign-compare"
+qmake build.pro -r QMAKE_CXXFLAGS_WARN_ON+="-Wno-unused-private-field -Wno-c++11-narrowing -Wno-deprecated-declarations -Wno-deprecated-register -Wno-nullability-completeness -Wno-sign-compare -Wno-inconsistent-missing-override"
 if test ! -f qwt/lib/libqwt.a; then make sub-qwt; fi
 make -j4 sub-src
