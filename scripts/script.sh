6538936e71e747ae60bc4cb70ca49dc297bf31a0
MA_PCUbuntu <19463568+peret2000@users.noreply.github.com>
2025-12-15 19:54:18 +0100

    Añade -j4 a make de qwt en script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -5,5 +5,5 @@ set -ev
 # Replica lo que se hace en appveyor.yml
 
 qmake build.pro -r QMAKE_CXXFLAGS_WARN_ON+="-Wno-unused-private-field -Wno-c++11-narrowing -Wno-deprecated-declarations -Wno-deprecated-register -Wno-nullability-completeness -Wno-sign-compare -Wno-inconsistent-missing-override"
-if test ! -f qwt/lib/libqwt.a; then make sub-qwt; fi
+if test ! -f qwt/lib/libqwt.a; then make -j4 sub-qwt; fi
 make -j4 sub-src
