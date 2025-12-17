3f97e881dcccfe46b69e1e08c1b54de5c35cc315
MA_Docker <19463568+peret2000@users.noreply.github.com>
2025-12-17 17:02:41 +0100

    Corrige d7a970a71: si no hay ergFile se produce un crash

--- src/Train/TrainSidebar.cpp
+++ src/Train/TrainSidebar.cpp
@@ -2092,7 +2092,7 @@ void TrainSidebar::guiUpdate()           // refreshes the telemetry
 
                 }
 
-                if (ergFile->hasGradient()) {
+                if (ergFile && ergFile->hasGradient()) {
                     // Average slope in 10 seconds (taking into account current speed)
 
                     int lap;
